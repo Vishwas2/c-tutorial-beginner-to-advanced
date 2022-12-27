93b5f41a9f7ecf0d899f50431e009ef6ceed0fc6
Vishwas2 <67436082+Vishwas2@users.noreply.github.com>
2022-12-27 22:59:36 +0530

    Add files via upload

---
+++ day03.c
@@ -0,0 +1,9 @@
+// keywords are reserved words in any language. They cannot be used as variable
+// some examples of keywords are int ,char , return, etc.
+// identifiers can be the name of any constant ,variable, etc. through which it can be identified
+// C is a case sensitive programing language( H and h are different )
+// a semi colon is used to terminate a statement
+// %d is used to display the output
+// a program in C is made up of keywords, identifiers, constants, string litrals and symbols
+
+
