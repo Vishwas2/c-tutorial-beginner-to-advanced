223739936a3f4b71ceb8306c12af28263fbd4616
Vishwas2 <67436082+Vishwas2@users.noreply.github.com>
2022-12-31 11:21:48 +0530

    Add files via upload

---
+++ day04.c
@@ -0,0 +1,26 @@
+// VARIABLES AND DATATYPES
+
+/*FLOW OF A C PROGRAM :
+Processing -> Compilation -> Assembly -> Linking -> Loading*/
+
+/* What is a variable?
+-> A name given to a memory location
+-> Declared by typing type variable_name;           eg: int a,b;
+-> Initialized and declared by type variable_name=value;        eg: int a=4; */
+
+/*RULES FOR DEFINING A VARIABLE IN C:
+-> Can contain alphabets,digits and underscore
+-> A variable name can start with alphabet or underscore only
+-> Can't start with a digit
+-> No whitespaces & reserved keywords are allowed
+-> Valid variable names: int vishwas, float vishwas123, char _vishwas25
+-> Invalid variable names: $vishwas, int 55vishwas, char long;*/
+
+/*
+-> Basic Data Types: int,char,float,double
+-> Derived Data Types: array,pointers,structure,union
+-> Enumeration Data Types: emum
+-> Void Data Type: void*/
+
+// 1 byte = 8 bits
+
