55f7d76cfb2a08693ed1ae8cbb133c538e878421
Vishwas Rana <67436082+Vishwas2@users.noreply.github.com>
2023-01-02 23:28:31 +0530

    Add files via upload

---
+++ day09.c
@@ -0,0 +1,13 @@
+// LOOPS IN C
+
+
+// Loops in programming are used to repeat a block of code until the specified condition is met.
+/*ADVANTAGES OF USING LOOPS:
+1) code reusability
+2) saves time
+3) traversing */
+
+/*TYPES OF LOOPS:
+1) do while loop
+2) while loop
+3) for loop */
