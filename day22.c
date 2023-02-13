cd7160629d5bdec45a9951d08a75ec6911ddb073
Vishwas Rana <67436082+Vishwas2@users.noreply.github.com>
2023-02-13 20:51:26 +0530

    Add files via upload

---
+++ day22.c
@@ -0,0 +1,9 @@
+// WHY IS RECURSION NOT ALWAYS GOOD ?
+
+/*
+-> Recursion is a good approach when it comes to problem solving.
+-> However, programmer needs to evaluate the need and impact of using iterative/recursive approach while solving a particular problem.
+-> In case of factorial calculation, recursion saved alot of lines of code.
+-> However in the case of fibonacci series, recursion resulted in some extra unnecessary function calls which was an extra overhead.
+-> Running time for fibonacci series grows exponentially with input in case of recursive approach.
+*/
